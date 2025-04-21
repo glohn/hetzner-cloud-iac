6047313b2d889f0b3528d46e05fb2a87a57d4117
Guido Lohn <glohn@rocketmail.com>
2025-04-21 14:43:04 +0200

    enable rescue mode and reboot

--- 01-tf/main.tf
+++ 01-tf/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.5.7"
+  required_version = ">= 1.10"
 
   required_providers {
     hcloud = {
@@ -32,14 +32,61 @@ provider "hcloud" {
 
 resource "hcloud_ssh_key" "default" {
   name       = "guido@local"
-  public_key = file("~/.ssh/id_ed25519.pub")
+  public_key = var.public_key
 }
 
 resource "hcloud_server" "test" {
-  name        = "tf-test"
-  server_type = "cx22"
+  name        = var.servername
+  server_type = var.server_type
   image       = "debian-12"
-  location    = "nbg1"
+  location    = var.location
   ssh_keys    = [hcloud_ssh_key.default.id]
+}
+
+resource "null_resource" "wait_for_ssh" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      while ! nc -z ${hcloud_server.test.ipv4_address} 22; do
+        sleep 2
+      done
+    EOT
+  }
+
+  depends_on = [hcloud_server.test]
+}
+
+resource "null_resource" "enable_rescue" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      ACTION_ID=$(curl -s -X POST \
+        -H "Authorization: Bearer ${var.hcloud_token}" \
+        -H "Content-Type: application/json" \
+        -d '{"type": "linux64", "ssh_keys": [${hcloud_ssh_key.default.id}]}' \
+        "https://api.hetzner.cloud/v1/servers/${hcloud_server.test.id}/actions/enable_rescue" | jq -r '.action.id')
+
+      while true; do
+        STATUS=$(curl -s \
+          -H "Authorization: Bearer ${var.hcloud_token}" \
+          "https://api.hetzner.cloud/v1/actions/$ACTION_ID" | jq -r '.action.status')
+        [ "$STATUS" = "success" ] && break
+        sleep 2
+      done
+    EOT
+  }
+
+  depends_on = [null_resource.wait_for_ssh]
+}
+
+resource "null_resource" "reboot_into_rescue" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      sleep 10
+      curl -s -X POST \
+        -H "Authorization: Bearer ${var.hcloud_token}" \
+        "https://api.hetzner.cloud/v1/servers/${hcloud_server.test.id}/actions/reboot"
+    EOT
+  }
+
+  depends_on = [null_resource.enable_rescue]
 }
 
---
+++ 01-tf/terraform.tfvars
@@ -0,0 +1,5 @@
+servername  = "tf-test"
+server_type = "cx22"
+location    = "nbg1"
+public_key  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKjbRFZ4QdMdB0Eku+SnPqVFkHFRIaE17a/nawmAYNfQ guido.user@nb335"
+
