14045c6528dabc15d98fde500022127ed7404366
Guido Lohn <glohn@rocketmail.com>
2025-05-02 16:17:05 +0200

    removed provider version

--- 00_tfstate/providers.tf
+++ 00_tfstate/providers.tf
@@ -10,26 +10,26 @@ terraform {
   # The backend configuration should initially be commented out.
   # Uncomment and run 'terraform init' to migrate the local state to the S3 bucket.
   # This should be done only after the code has been run successfully once.
-  #backend "s3" {
-  #  bucket = "yourname-hetzner-lab-tfstate"
-  #  key    = "tfstate.tfstate"
-  #  region = "main"
-  #  endpoints = {
-  #    s3 = "https://nbg1.your-objectstorage.com"
-  #  }
+  backend "s3" {
+    bucket = "yourname-hetzner-lab-tfstate"
+    key    = "tfstate.tfstate"
+    region = "main"
+    endpoints = {
+      s3 = "https://nbg1.your-objectstorage.com"
+    }
 
-  #  profile                     = "hetzner-s3-tfstate"
-  #  skip_credentials_validation = true
-  #  skip_metadata_api_check     = true
-  #  skip_region_validation      = true
-  #  skip_requesting_account_id  = true
-  #  skip_s3_checksum            = true
-  #  use_path_style              = true
-  #}
-
-  backend "local" {
-    path = "terraform.tfstate"
+    profile                     = "hetzner-s3-tfstate"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
   }
+
+  #backend "local" {
+  #  path = "terraform.tfstate"
+  #}
 }
 
 provider "minio" {
