3e76942abdc2af937c44ace4bdec8ef74bcce331
Guido Lohn <glohn@rocketmail.com>
2025-05-02 16:01:38 +0200

    added cloud-init

---
+++ tf-modules/services/vm/scripts/install_docker.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt remove -y docker.io docker-doc docker-compose podman-docker containerd runc
+sudo apt install -y gnupg-agent software-properties-common
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+sudo echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
